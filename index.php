5fa674640f25d782af842cecaccc0de5d1e30272
Rodrigo <yareddelgado18@gmail.com>
2024-06-12 16:49:10 -0600

    agregre el index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="functions.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Juego</title>
+</head>
+<body>
+    <div id="Container">
+        <header id="Still">
+            <h1 id="title">Juego</h1>
+            <button id="btnInciar" type="submit">Iniciar el juego</button>
+        </header>
+    </div>
+</body>
+</html>
