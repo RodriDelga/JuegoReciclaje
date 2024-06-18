cc0b97782ca61e7c7cee40ed687aa6105d120bb2
Rodrigo <yareddelgado18@gmail.com>
2024-06-18 17:51:28 -0600

    Mejorando la pagina

--- ProyectoEcologia/src/PaginaPrincipal.jsx
+++ ProyectoEcologia/src/PaginaPrincipal.jsx
@@ -1,18 +1,32 @@
 import './PaginaPrincipal.css'
 
 export const PaginaPrincipal = () => {
-
+    
 
     return (
         <div className="Main-Container">
             <header className="Main-header">
-                <div className="Header-Container">
-                    <div className="">
-                        <h1>Reciclaje</h1>
+                <div className="logo">
+                    <img src='src\images\CBTIS49.png' className='logo-img'/>
+                    <h1 className='logo-name'>Reciclaje</h1>
+                </div>
+                <div className='Information'>
+                    <div className='Info-Container'>
+                        <h2>Incio</h2>
+                    </div>
+                    <div className='Info-Container'>
+                        <h2 className='Who'>¿Quienes somos?</h2>
+                    </div>
+                    <div className='Info-Container'>
+                        <h2>Juego</h2>
                     </div>
                 </div>
             </header>
-            <button>Hola</button>
+            <section className='main-section'>
+                <article className='main-text'>
+                    <h1>Bienvenido a nuestra pagina</h1>
+                </article>
+            </section>
         </div>
     )
 }
